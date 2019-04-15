d259ba0d1391d1f73f73d67eece7c26ee38347bb
Lwj1423 <m17621954542@163.com>
2019-04-15 08:54:57 +0800

    LWJ

--- src/main/java/com/example/demo/config/MyServerConfig.java
+++ src/main/java/com/example/demo/config/MyServerConfig.java
@@ -43,15 +43,15 @@ public class MyServerConfig {
     }
 
     //配置嵌入式的Servlet容器
-    @Bean
-    public WebServerFactoryCustomizer<ConfigurableWebServerFactory> webServerFactoryCustomizer(){
-        return new WebServerFactoryCustomizer<ConfigurableWebServerFactory>() {
-            //定制嵌入式的Servlet容器相关的规则
-            @Override
-            public void customize(ConfigurableWebServerFactory factory) {
-                factory.setPort(8083);
-            }
-        };
-    }
+//    @Bean
+//    public WebServerFactoryCustomizer<ConfigurableWebServerFactory> webServerFactoryCustomizer(){
+//        return new WebServerFactoryCustomizer<ConfigurableWebServerFactory>() {
+//            //定制嵌入式的Servlet容器相关的规则
+//            @Override
+//            public void customize(ConfigurableWebServerFactory factory) {
+//                factory.setPort(8083);
+//            }
+//        };
+//    }
 
 }
